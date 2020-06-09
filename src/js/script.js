453c7c08f30edc3784ae2892e7227654564f9732
Владимир <razzorenoff@gmail.com>
2020-06-09 23:54:05 +0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,30 @@
+
+let ready = (callback) => {
+	if (document.readyState != "loading") callback();
+	else document.addEventListener("DOMContentLoaded", callback);
+}
+
+
+ready(() => {
+
+	//MENU
+		let iconMenu=document.querySelector(".icon-menu");
+		let body=document.querySelector("body");
+		let menuBody=document.querySelector(".menu__body");
+	iconMenu.addEventListener("click", (e) => {
+		iconMenu.classList.toggle("active");
+		body.classList.toggle("lock");
+		menuBody.classList.toggle("active");
+	});
+
+
+	//IBG
+	document.querySelectorAll(".ibg").forEach(el => {
+		if(el.querySelector('img')){
+			el.style.backgroundImage = 'url('+el.querySelector('img').getAttribute('src')+')';
+			el.querySelector('img').style.display = 'none';
+		}
+	});
+});
+
+
